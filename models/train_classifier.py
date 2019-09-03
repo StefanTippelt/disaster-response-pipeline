025adf7d512fb063b247fee02420d8ebb3487750
StefanTippelt <StefanTippelt@users.noreply.github.com>
2019-09-03 22:52:14 +0200

    feat: Add app structure

---
+++ models/train_classifier.py
@@ -0,0 +1,85 @@
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+import nltk
+from sklearn.metrics import f1_score
+from nltk.tokenize import sent_tokenize, word_tokenize
+# nltk.download('punkt')
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('desaster_response', con=engine)
+    X = df["message"]
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
+
+
+def tokenize(text):
+    return word_tokenize(text)
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
+    ])
+
+    parameters = {
+            'clf__estimator__min_samples_split': [5,10, 15],
+            'clf__estimator__n_estimators': [50, 100, 150]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, scoring='accuracy', verbose=1, n_jobs=-1)    
+    return cv    
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_true=y_test, y_pred=y_pred, target_names=target_names))
+    return None
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as file:  
+        pickle.dump(model, file)
+    return None
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print(f'Loading data...\n    DATABASE: {database_filepath}')
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print(f'Saving model...\n    MODEL: {model_filepath}')
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
